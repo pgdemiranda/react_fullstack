d7d8dd1ccb306773fca021d35695ed61ad2986f2
Pablo Miranda <pgdemiranda@gmail.com>
2024-06-18 15:18:21 -0300

    exercícios 05 e 05_01 feitos com consulta

---
+++ 1.react_com_referencias/src/Exercicio05.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TrabalhandoComImagens from './components/TrabalhandoComImagens'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <TrabalhandoComImagens />
+    </div>
+  )
+}
+
+export default App
